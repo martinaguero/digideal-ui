54dadfc4c685fedc0f4637e6d55bb5217af9cbae
martinaguero <aguero.martin@gmail.com>
2019-08-11 20:09:03 -0300

    currency combo fix

--- web/src/org/trimatek/digideal/ui/beans/ContractView.java
+++ web/src/org/trimatek/digideal/ui/beans/ContractView.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
